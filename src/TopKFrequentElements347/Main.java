371d494b5ae43964267a2583dcb5c8b97f6f460e
Saul <saullionlee@hotmail.com>
2022-07-26 00:02:56 -0600

    Update #347 comments

--- src/TopKFrequentElements347/Main.java
+++ src/TopKFrequentElements347/Main.java
@@ -1,13 +1,18 @@
 package TopKFrequentElements347;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args) {
         Solution1 mySolution2 = new Solution1();
         int k = 2;
-        int[] nums = new int[]{2,2,2,3,2,3,3,1,1,1};
+        int[] nums = new int[]{3,1,2,2,2,3};
         int [] result = mySolution2.topKFrequent(nums,k);
-       for(int i : result){
+        for(int i : result){
            System.out.println(i);
-       }
+        }
     }
 }
--- src/TopKFrequentElements347/Solution2.java
+++ src/TopKFrequentElements347/Solution2.java
@@ -1,6 +1,7 @@
 package TopKFrequentElements347;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.PriorityQueue;
 
 public class Solution2 {
@@ -14,18 +15,20 @@ public class Solution2 {
         for(int num : nums){
             map.put(num, map.getOrDefault(num,0) + 1);
         }
+        //pq add nums in Ascending order, so the frequency is sorted
         PriorityQueue<Integer> pq = new PriorityQueue<>(
                 (n1, n2) -> map.get(n1) - map.get(n2)
         );
-
+        //fill the pq with keys in the hashmap
         for(Integer key : map.keySet()){
             pq.offer(key);
             if(pq.size() > k){
-                pq.poll();
+                pq.poll();//FIFO, the smallest got removed firstly
             }
         }
+
         int [] result = new int[k];
-        for(int i = 0; i < result.length; i++){
+        for(int i = 0; i < k; i++){
             result[i] = pq.poll();
         }
         return result;
