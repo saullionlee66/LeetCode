971200e473116d0075191075d8470e3e87848444
Saul <saullionlee@hotmail.com>
2022-07-27 23:27:20 -0600

    Update #15 and #167

---
+++ src/threeSum15/Main.java
@@ -0,0 +1,10 @@
+package threeSum15;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution mySolution = new Solution();
+        int[] nums = new int[]{1,2,1,-1,-3,-2,0,3};
+
+        System.out.println(mySolution.threeSum(nums));
+    }
+}
