77e7812b4af17515286cdfd0d3ce2316a5f82f9a
Saul <saullionlee@hotmail.com>
2022-07-28 23:35:26 -0600

    Add #424

---
+++ src/LongestRepeatingCharacterReplacement424/Main.java
@@ -0,0 +1,10 @@
+package LongestRepeatingCharacterReplacement424;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution mySolution = new Solution();
+        String s = "ABBABCCCQA";
+        int k = 4;
+        System.out.println(mySolution.characterReplacement(s,k));
+    }
+}
